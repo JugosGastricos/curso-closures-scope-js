bbba1defe7f8fdca7fc4591b9f86b7a4d3cd9ac3
Santiago Alvarez <santiagoalvarez9617@gmail.com>
2022-07-14 21:08:09 -0500

    Clase 5: se aprendió y practicó acerca de reasignación y redeclaración utilizando var, let y const

---
+++ src/scope/reassign.js
@@ -0,0 +1,31 @@
+//Usando var:
+var firstName; //declaración
+firstName = 'Oscar'; //asignación
+console.log(firstName);
+
+var lastName = 'David'; //declaración y asignación
+lastName = 'Ana'; //reasignación
+console.log(lastName);
+
+var secondName = 'David';
+var secondName = 'Ana'; //redeclaración
+console.log(secondName);
+
+//Usando let
+let fruit = 'Apple'; //delaración y asignación
+fruit = 'Kiwi'; //reasignación
+console.log(fruit);
+
+let fruit = 'Banana'; //redeclaración
+console.log(fruit);
+
+//Usando const
+const animal = 'dog'; //delaración y asignación
+animal = 'cat'; //reasignación
+console.log(animal);
+
+const animal = 'snake'; //redeclaración
+
+const vehicles = [];
+vehicles.push('🚗');
+console.log(vehicles);
