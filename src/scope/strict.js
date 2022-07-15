b7053c92378587dea3c7c3c52ed2138da97d5b80
Santiago Alvarez <santiagoalvarez9617@gmail.com>
2022-07-14 21:28:39 -0500

    Clase 6: se aprendió a utilizar el modo estricto en scripts y en funciones

---
+++ src/scope/strict.js
@@ -0,0 +1,11 @@
+'use strict';
+pi = 3.1416; //asignación sin declaración
+console.log(pi);
+
+function myFunction(){
+    'use strict';
+    pi = 3.1416; //asignación sin declaración
+    console.log(pi);
+}
+
+myFunction();
