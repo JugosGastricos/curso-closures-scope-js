654f002a78cfdd6704ff619c36033716af3aabf4
Santiago Alvarez <santiagoalvarez9617@gmail.com>
2022-07-15 01:10:22 -0500

    Realización de ejercicio para entender mejor la clase 8: Practicando Closures

--- src/closure/mymoneybox.js
+++ src/closure/mymoneybox.js
@@ -26,3 +26,35 @@ const moneyBoxAna = moneyBox();
 moneyBoxAna(10);
 moneyBoxAna(20);
 moneyBoxAna(5);
+
+//experimento
+
+function productos() {
+    let agregarCarrito = [];
+
+    function agregarProducto(producto) {
+        agregarCarrito.push(producto);
+    }
+    function eliminarProducto(producto){
+        agregarCarrito.splice(agregarCarrito.findIndex((index) => index === producto), 1);
+    }
+    function mostrarProductos(){
+        console.log(agregarCarrito);
+    }
+    return {
+        agregarProducto,
+        eliminarProducto,
+        mostrarProductos,
+    };
+}
+
+const carrito = productos();
+carrito.agregarProducto('Galleta');
+carrito.agregarProducto('Chocolate');
+carrito.agregarProducto('Avena');
+carrito.agregarProducto('Leche');
+carrito.agregarProducto('Azúcar');
+carrito.mostrarProductos();
+carrito.eliminarProducto('Chocolate');
+carrito.eliminarProducto('Leche');
+carrito.mostrarProductos();
